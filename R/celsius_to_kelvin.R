ad9161223a4e940a108a5ef70ab1d003432628de
Suwetha <igsuwetha@gmail.com>
2023-11-09 14:45:12 -0800

    roxygen comments added for the celsius_to_kelvin function

--- R/celsius_to_kelvin.R
+++ R/celsius_to_kelvin.R
@@ -1,4 +1,17 @@
-# Convert Celsius to Kelvin
+# Convert temperature from Celsius to Kelvin
+#'
+#'@description Given an input temperature number, this function would convert
+#'it from celsius to kelvin.
+#'
+#'@param celsius A number
+#'
+#'@return Number representing temperature in kelvin
+#'
+#'@export
+#'
+#'@examples
+#'celsius_to_kelvin(10)
+#'celsius_to_kelvin(0)
 celsius_to_kelvin <- function(celsius) {
   return(celsius + 273.15)
 }
